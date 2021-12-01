973c8540c83dbc89fd6ef6ddf365ef9e2ac98055
David <david@nlsn.nu>
2021-12-01 15:08:51 +0100

    Initial project files

---
+++ src/main/java/com/example/cleanNiceAB/CleanNiceAbApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanNiceAB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanNiceAbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanNiceAbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cleanNiceAB/CleanNiceAbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanNiceAB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanNiceAbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

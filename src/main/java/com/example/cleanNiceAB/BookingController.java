46f87481e7ddf4851c4d14990ba012fc2b440c8c
Sandra Wahlgren <71629445+Swala@users.noreply.github.com>
2021-12-03 17:02:38 +0100

    write to DB works from frontend. By Ludvig, David & Sandra

--- src/main/java/com/example/cleanNiceAB/BookingController.java
+++ src/main/java/com/example/cleanNiceAB/BookingController.java
@@ -5,30 +5,29 @@ import com.example.cleanNiceAB.entities.Booking;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.ls.LSOutput;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/booking")
 public class BookingController {
 
+
     private final BookingService bookingService;
 
     @Autowired
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
+
     }
 
-    @GetMapping("/add")
+    @PostMapping("/add")
     public ResponseEntity<Booking> addBooking(@RequestBody Booking booking){
+        System.out.println(booking.getClass().getName());
         Booking newBooking = bookingService.addBooking(booking);
         return new ResponseEntity<>(newBooking, HttpStatus.CREATED);
     }
 
-    @GetMapping("/api/start")
-    public String test(){
-        System.out.println("test");
-        return "this is a test";
-
-    }
 }

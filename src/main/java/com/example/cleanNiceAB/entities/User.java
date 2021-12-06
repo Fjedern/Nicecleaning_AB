b58d94d3fd6ca86beba298795e4fd91cbbbb76e5
JonasHolmkvist <jonas_holmkvist@hotmail.se>
2021-12-06 12:46:04 +0100

    Add new user

---
+++ src/main/java/com/example/cleanNiceAB/entities/User.java
@@ -0,0 +1,36 @@
+package com.example.cleanNiceAB.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "f_name")
+    private String fName;
+    @Column(name = "l_name")
+    private String lName;
+    @Column(name = "phone_nr")
+    private String phoneNr;
+    @Column(name = "address")
+    private String address;
+
+}
---
+++ src/main/java/com/example/cleanNiceAB/repos/UserRepo.java
@@ -0,0 +1,19 @@
+package com.example.cleanNiceAB.repos;
+
+
+import com.example.cleanNiceAB.entities.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    void deleteUserById(Long id);
+
+    Optional<User> findById(Long id);
+}

c71c2c16b5e204dbfc72038955ab8498be9f7652
JonasHolmkvist <jonas_holmkvist@hotmail.se>
2021-12-07 10:41:01 +0100

    pull

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,8 +2,38 @@
 import BookingForm from '../components/BookingForm.js';
 import AddUserForm from '../components/AddUserForm.js';
 import { Link } from "react-router-dom";
+import {useEffect} from "react";
 
 const MainPage =()=>{
+
+    const postUserData = {
+        "id": 0,
+        "userName": "firstUser",
+        "password": "first123",
+        "fName": "First",
+        "lName": "Firstsson",
+        "phoneNr": "123456",
+        "address": "Firstgatan 1, Firststad"
+    };
+
+    useEffect(() => {
+        fetch("http://localhost:8080/user/add", {
+                method: "post",
+                headers: {"Content-Type": 'application/json'},
+                body: JSON.stringify(postUserData),
+            }
+        )
+            .then(response => {
+                console.log(response.status)
+                if (response.status === 201) {
+                    console.log(response);
+
+                } else {
+                    console.log(response.status)
+                }
+            })
+            .catch(err => err)
+    }, []);
 
 
     return (

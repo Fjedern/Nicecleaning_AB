fa8e6c45fcf6ad16a21d9f979690a72ceac598df
Ludvig Anderbeck <ludviganderbeck@gmail.com>
2021-12-09 08:58:29 +0100

    login returns a jwt

--- src/main/java/com/example/cleanNiceAB/LoginController.java
+++ src/main/java/com/example/cleanNiceAB/LoginController.java
@@ -3,8 +3,10 @@ package com.example.cleanNiceAB;
 import com.example.cleanNiceAB.Services.UserService;
 import com.example.cleanNiceAB.entities.User;
 import com.example.cleanNiceAB.exeptions.NoSuchUserNameOrPasswordException;
+import com.example.cleanNiceAB.utils.JwtUtils;
 import com.example.cleanNiceAB.utils.SecureUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,20 +23,20 @@ public class LoginController {
     UserService userService;
 
     @PostMapping("/validation")
-    public RedirectView loginValidator(@RequestBody String userName, String password) {
+    public ResponseEntity<?> loginValidator(@RequestBody String userName, String password) {
         List<User> userList = userService.getAll();
 
         for (User user: userList){
             if (user.getEmail().equals(userName)) {
                 password = SecureUtils.getSecurePassword(password, user.getSalt());
                 if (user.getPassword().equals(password)) {
-                    return new RedirectView("http://localhost:3000/minsida");
-
+                    final String token = JwtUtils.createJWT(user);
+                    return ResponseEntity.ok(new JwtResponse(token));
                 }
             }
         }
 
-        return new RedirectView("http://localhost:3000/");
+        return null;
 
     }
 }
---
+++ src/main/java/com/example/cleanNiceAB/utils/JwtUtils.java
@@ -0,0 +1,48 @@
+package com.example.cleanNiceAB.utils;
+
+import com.example.cleanNiceAB.entities.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static String secret = "Dammsuga";
+
+    public static String createJWT(User user){
+
+        SignatureAlgorithm sa = SignatureAlgorithm.HS256;
+
+        Long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        Date expire = new Date(nowMillis + 10800000);
+
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secret);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, sa.getJcaName());
+
+        JwtBuilder builder = Jwts.builder()
+                .setId(user.getId().toString())
+                .setIssuedAt(now)
+                .setIssuer("StädaFint AB")
+                .setSubject("user info")
+                .signWith(sa, signingKey)
+                .setExpiration(expire);
+
+        return builder.compact();
+    }
+
+    public static Claims decodeJWT(String jwt) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(secret))
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+}

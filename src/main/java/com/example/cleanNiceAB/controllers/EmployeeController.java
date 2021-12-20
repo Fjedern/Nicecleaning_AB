9712bf93473a09f5f93068a8954da0d40f4e94c2
Ludvig Anderbeck <ludviganderbeck@gmail.com>
2021-12-20 11:49:54 +0100

    dto added in controller

--- src/main/java/com/example/cleanNiceAB/controllers/EmployeeController.java
+++ src/main/java/com/example/cleanNiceAB/controllers/EmployeeController.java
@@ -3,17 +3,18 @@ package com.example.cleanNiceAB.controllers;
 import com.example.cleanNiceAB.Services.EmployeeService;
 import com.example.cleanNiceAB.entities.Booking;
 import com.example.cleanNiceAB.entities.Employee;
-import com.example.cleanNiceAB.entities.User;
 import com.example.cleanNiceAB.utils.JwtUtils;
 import com.example.cleanNiceAB.utils.SecureUtils;
 import io.jsonwebtoken.Claims;
 import lombok.AllArgsConstructor;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -38,8 +39,13 @@ public class EmployeeController {
     }
 
     @GetMapping("/viewAll")
-    public List<Employee> getAllEmployees(@RequestHeader String token){
-        return employeeService.findAllEmployees();
+    public List<EmployeeDTO> getAllEmployees(@RequestHeader String token){
+        List<Employee> employeeList = employeeService.findAllEmployees();
+        List<EmployeeDTO> employeeDTOs = new ArrayList<>();
+        for (Employee employee : employeeList){
+            employeeDTOs.add(new EmployeeDTO(String.valueOf(employee.getId()),employee.getName(),employee.getAddress(),employee.getPhoneNr(),employee.getEmail()));
+        }
+        return employeeDTOs;
     }
 
     @GetMapping("/viewBookings")
@@ -48,5 +54,16 @@ public class EmployeeController {
         Optional<Employee> optionalEmployee = employeeService.findEmployeeById(Long.parseLong(claims.getId()));
         Employee employee = optionalEmployee.get();
         return employee.getBookings();
+    }
+
+    @Value
+    public static class EmployeeDTO {
+        String employeeId;
+        String name;
+        String address;
+        String phoneNr;
+        String email;
+
+
     }
 }
